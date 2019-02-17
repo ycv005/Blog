bc0b85d47fba91f861c1c09ec443b52cf88232b7
ycv005 <ycverma005@gmail.com>
2019-02-18 01:18:35 +0530

    Changed html for blog

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views #getting all of views from the blog application
+
+urlpatterns= [
+    path('',views.post_list,name="post_list"), #assigning the view called post_list to the root url. "" signify the root url
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. Views play the important logic, it will take request from the model
+#pass it to the template(blog/post_list.html)
+
+def post_list(request):
+    return render(request,'blog/post_list.html',{})
