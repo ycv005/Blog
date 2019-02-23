cb4680953315d966f825f76f84724b03ab55f163
ycv005 <ycverma005@gmail.com>
2019-02-24 02:02:09 +0530

    Added view & template for blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views #getting all of views from the blog application
 
 urlpatterns= [
     path('',views.post_list,name="post_list"), #assigning the view called post_list to the root url. "" signify the root url
+    path('post/<int:pk>/',views.post_detail,name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render,get_object_or_404
 # Create your views here. Views play the important logic, it will take request from the model
 #pass it to the template(blog/post_list.html)
 
 def post_list(request):
     posts= Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request,'blog/post_detail.html',{'post': post})
