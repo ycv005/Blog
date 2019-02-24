c9c1fcfffef4c65d90ab106b69896885e7e11fb5
ycv005 <ycverma005@gmail.com>
2019-02-24 14:47:15 +0530

    Added the New/Edit options for Post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from . import views #getting all of views from the blog application
+from django.urls import path 
+from . import views
 
-urlpatterns= [
-    path('',views.post_list,name="post_list"), #assigning the view called post_list to the root url. "" signify the root url
-    path('post/<int:pk>/',views.post_detail,name='post_detail')
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
